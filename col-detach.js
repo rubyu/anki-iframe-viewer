29d90ed1362184d1b6773d8b835106f7959f51be
rubyu <ruby.u.g@gmail.com>
2016-03-24 20:04:10 +0900

    add AudioPlayer

--- col-detach.js
+++ col-detach.js
@@ -133,6 +133,41 @@ window.onload = function() {
       
     };
     
+    var AudioPlayer = function() {
+      var audio = document.getElementsByTagName("audio");
+      if (audio.length > 0) {
+        this.audio = audio[0];
+      }
+      this.remaining = 0;
+    };
+    AudioPlayer.prototype.load = function() {
+      if (this.audio) {
+        this.audio.loop = false;
+        this.audio.load();
+      }
+    };
+    AudioPlayer.prototype.playOnce = function() {
+      var self = this;
+      this.remaining += 1;
+      console.log("remaining: %d", this.remaining);
+      if (this.audio && !this.setup) {
+        console.log("setup");
+        this.setup = true;
+        this.audio.addEventListener("ended", function() {
+          self.remaining -= 1;
+          console.log("remaining: %d", self.remaining);
+          if (self.remaining > 0) {
+            self.audio.play();
+          }
+        }, false);
+      }
+      if (this.audio && this.remaining == 1) {
+        console.log("play");
+        this.audio.play();
+      }
+    };
+    
+    
     var Flash = function() {
       var container = document.createElement("div");
       container.id = "flash-container";
@@ -198,8 +233,9 @@ window.onload = function() {
     };
     
     
-    var TouchEvent = function(viewer) {
+    var TouchEvent = function(viewer, audioPlayer) {
       this.viewer = viewer;
+      this.audioPlayer = audioPlayer;
     };
     TouchEvent.prototype.up = function() {
       this.viewer.goNextPage();
@@ -226,16 +262,17 @@ window.onload = function() {
       this.viewer.goPrevField();
     };
     TouchEvent.prototype.tap = function() {
-      //alert("tap");
+      this.audioPlayer.playOnce();
     };
     
-    var Dispacher = function(touchEvent) {
+    var Dispacher = function(touchEvent, preferredDispacher) {
       this.touchEvent = touchEvent;
       this.state = {};
       var _1em = this._getOneEmInPixels();
       this._minShortSwipeSize = _1em * 3;
       this._minLongSwipeSize = Math.floor(Math.min(screen.height, screen.width) * 0.4);
-      
+      this._preferredDispatcher = preferredDispacher;
+      this.lastDispatchTime = Date.now();
     };
     Dispacher.prototype._getOneEmInPixels = function() {
       var elem = document.createElement('div');
@@ -248,9 +285,14 @@ window.onload = function() {
     Dispacher.prototype.dispatchStart = function(x, y) {
       this.state.x = x;
       this.state.y = y;
-      
     };
     Dispacher.prototype.dispatchEnd = function(x, y) {
+      this.lastDispatchTime = Date.now();
+      // prevent duplicate fire of touchend and mouseup event
+      if (this._preferredDispatcher && 
+          this.lastDispatchTime - this._preferredDispatcher.lastDispatchTime < 1500) {
+        return;
+      }
       var _x = this.state.x;
       var _y = this.state.y;
       var dx = _x - x;
@@ -293,6 +335,8 @@ window.onload = function() {
     };
     
     var flash = new Flash();
+    var audioPlayer = new AudioPlayer();
+    audioPlayer.load();
     var viewer = new Viewer(flash);
     // detached-00 is `head`
     // detached-01 is `COCA`
@@ -303,9 +347,9 @@ window.onload = function() {
     viewer.registerField(document.getElementById("detached-06"), "ロングマン現代英英辞典");
     viewer.registerField(document.getElementById("detached-07"), "斎藤和英大辞典");
     viewer.castCurrentState();
-    var touchEvent = new TouchEvent(viewer);
-    var mouseDispacher = new Dispacher(touchEvent);
+    var touchEvent = new TouchEvent(viewer, audioPlayer);
     var touchDispacher = new Dispacher(touchEvent);
+    var mouseDispacher = new Dispacher(touchEvent, touchDispacher);
     
     //https://developer.mozilla.org/ja/docs/Web/Guide/DOM/Events/Touch_events
     document.addEventListener('mousedown', function(event) {
