bfa794a19a18477fa5d73469a86c6bcb07ebd336
rubyu <ruby.u.g@gmail.com>
2016-03-27 17:57:48 +0900

    remove unused loading of woothee.js

--- col-detach.js
+++ col-detach.js
@@ -1,14 +1,5 @@
 "use strint;";
 
-function loadScript(url) {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = url;
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(script, s);
-}
-loadScript("woothee.js");
-
 (function() {
   var defaultFontSize = 14;
   function getAdjustedFontSize() {
