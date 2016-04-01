2b5d30aca0ad9329080076e41580e51b6de71c73
rubyu <ruby.u.g@gmail.com>
2016-04-01 17:12:47 +0900

    separate the project into sub projects of dev and prod

--- project/Build.scala
+++ project/Build.scala
@@ -1,31 +1,71 @@
 import sbt._
 import Keys._
+import org.scalajs.sbtplugin._
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 
-object JSBuild extends Build {
-  override def projects = Seq(root)
+object Build extends sbt.Build {
+  lazy val commonSettings =
+    Defaults.coreDefaultSettings ++
+      Seq(
+        version := "1.0",
+        scalaVersion := "2.11.8",
+        name := "anki-iframe-viewer"
+      )
 
-  lazy val root = Project("root", file("."),
-    settings = Defaults.coreDefaultSettings ++ rootSettings)
+  lazy val dev = Project("dev", file(".")).
+    settings(commonSettings: _*).
+    settings(Seq(
+      name := "anki-iframe-viewer-dev"
+    )).
+    settings(Seq(
+      target := file("./target/dev/")
+    )).
+    settings(Seq(
+      dist := distTask.dependsOn(fullOptJS in Compile).value
+    )).
+    settings(
+      libraryDependencies ++= Seq(
+        "org.scala-js" %%% "scalajs-dom" % "0.9.0",
+        "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test"
+      )
+    ).
+    enablePlugins(ScalaJSPlugin)
 
-  lazy val rootSettings = Seq(
-    dist := {
-      import IO._
-      val dist = file("./dist/")
-      val resources = file("./src/main/resources/")
-      val files = Seq(
-        ("./target/scala-2.11/anki-iframe-viewer-opt.js",     s"$dist/anki-iframe-viewer.js"),
-        ("./target/scala-2.11/anki-iframe-viewer-opt.js.map", s"$dist/anki-iframe-viewer-opt.js.map")
-      ) map {case (a, b) =>
-        (file(a), file(b))
-      }
-      if (dist.exists && dist.isFile) {
-        throw new IllegalStateException(s"'$dist' is not a directory")
-      }
-      dist.delete()
-      dist.mkdir()
-      copyDirectory(resources, dist, overwrite = true, preserveLastModified = true)
-      copy(files, overwrite = true, preserveLastModified = true)
+  lazy val prod = Project("prod", file(".")).
+    settings(commonSettings: _*).
+    settings(Seq(
+      target := file("./target/prod/")
+    )).
+    settings(Seq(
+      dist := distTask.dependsOn(fullOptJS in Compile).value
+    )).
+    settings(Seq(
+      scalacOptions := Seq("-Xdisable-assertions")
+    )).
+    settings(
+      libraryDependencies ++= Seq(
+        "org.scala-js" %%% "scalajs-dom" % "0.9.0",
+        "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test"
+      )
+    ).
+    enablePlugins(ScalaJSPlugin)
+
+  val dist = TaskKey[Unit]("dist")
+
+  private def distTask = Def.task {
+    import IO._
+    val dist = file("./dist/")
+    val resources = file("./src/main/resources/")
+    val files = crossTarget.value.listFiles.collect {
+      case f if f.isFile && f.name.contains("opt.js") =>
+        (f, file(s"$dist/${f.name}"))
     }
-  )
-  lazy val dist = TaskKey[Unit]("dist")
+    if (dist.exists && dist.isFile) {
+      throw new IllegalStateException(s"'$dist' is not a directory")
+    }
+    dist.delete()
+    dist.mkdir()
+    copyDirectory(resources, dist, overwrite = true, preserveLastModified = true)
+    copy(files, overwrite = true, preserveLastModified = true)
+  }
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,35 @@
-package com.github.rubyu.anki.iframe.viewer
+
 
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.Dynamic.global
+import scala.scalajs.js.annotation.JSExport
+import org.scalajs.dom.{window, document}
+import org.scalajs.dom.html._
 
-object IframeViewer extends JSApp {
+@JSExport
+object AnkiIframeViewer extends JSApp {
+  def isAnki = window.navigator.userAgent.contains("Anki")
+
+  def toStandardMode(): Unit = {
+    val html = document.getElementsByTagName("html")(0).asInstanceOf[Html]
+    val body = document.getElementsByTagName("body")(0).asInstanceOf[Body]
+    html.style.overflowY = "auto"
+    body.style.overflowY = "auto"
+  }
+
+  @JSExport
   def main(): Unit = {
-    println("hello!")
+    if (isAnki) {
+      this.toStandardMode()
+      global.console.log("anki-iframe-viewer cannot work on old browsers :(")
+      println("anki-iframe-viewer cannot work on old browsers :(")
+      return
+    }
+
+    assert(false)
+
+    global.console.log("ok")
+    println("ok")
+
   }
 }
