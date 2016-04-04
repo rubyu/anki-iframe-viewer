c09a8f9e796eb59423b6ef332da6e6b55b9a6292
rubyu <ruby.u.g@gmail.com>
2016-04-05 00:13:46 +0900

    improve the behavior of Viewer's chapter/page functions on it contains empty chapter list

--- src/main/scala/Viewer.scala
+++ src/main/scala/Viewer.scala
@@ -18,66 +18,88 @@ class Viewer(flash: Flash, chapters: List[Chapter]) extends Logger {
   def ordinaryPosition(m: Double): Double = {
     val n = viewSize
     m % n match {
-      case q if q > 0 => math.floor((m / n) + 0.5) * n
-      case q => q * n
+      case q if q > 0 =>
+        debug(f"m: $m, n: $n, q: $q>0 ordinaryPosition: ${ math.floor((m / n) + 0.5) * n }")
+        math.floor((m / n) + 0.5)
+      case q =>
+        debug(f"m: $m, n: $n, q: $q==0 ordinaryPosition: $m")
+        m
     }
   }
   def setPosition(pos: Double, caption: String): Unit = setPosition(pos, Some(caption))
   def setPosition(pos: Double): Unit = setPosition(pos, None)
   def setPosition(pos: Double, caption: Option[String]): Unit = {
+    debug(f"set position: $pos")
     window.scrollTo(pos.toInt, 0)
     flash.cast(caption)
   }
-  def findActiveChapter(list: Iterable[Chapter]): LogicalChapter = {
+  def findActiveChapter(list: Iterable[Chapter]): Option[LogicalChapter] = {
     val pos = ordinaryPosition(position)
     list.find { c =>
       if (pos >= c.offsetLeft && pos <= c.offsetLeft + c.offsetWidth) true
       else false
-    } getOrElse {
-      if (pos < chapters.head.offsetLeft) StartOfCard
-      else EndOfCard
+    } orElse {
+      if (list.isEmpty) {
+        if (position < document.body.scrollWidth / 2) Option(StartOfCard)
+        else Option(EndOfCard)
+      } else {
+        if (pos < chapters.head.offsetLeft) Option(StartOfCard)
+        else Option(EndOfCard)
+      }
     }
   }
   def atFirstPageOf(c: Chapter) = position == c.offsetLeft
   def atLastPageOf(c: Chapter) = position > c.offsetLeft + c.offsetWidth - viewSize
   def go(chapter: LogicalChapter): Unit = {
+    debug(f"go: $chapter")
     chapter match {
-      case EndOfCard =>
-      case c =>
+      case c: Chapter =>
         val p = c.offsetLeft
         val op = ordinaryPosition(p)
         if (p != op) {
           error(f"given position is not ordinary: $p; nearest ordinary position: $op")
         }
         setPosition(op, c.caption)
+      case c => setPosition(c.offsetLeft, c.caption)
     }
   }
   def goPrevPage(): Unit = setPosition(ordinaryPosition(position) - viewSize)
   def goNextPage(): Unit = setPosition(ordinaryPosition(position) + viewSize)
   def goPrevChapter(): Unit = {
     findActiveChapter(chapters) match {
-      case c: Chapter =>
+      case Some(c: Chapter) =>
         if (atFirstPageOf(c)) {
           if (c == chapters.head) go(StartOfCard)
           else go(chapters(chapters.indexOf(c)-1))
         }
-      case c => go(c)
+      case Some(StartOfCard) => go(StartOfCard)
+      case Some(EndOfCard) =>
+        if (chapters.isEmpty) go(StartOfCard)
+        else go(chapters.last)
+      case None => fatal(f"active chapter was not found")
     }
   }
   def goNextChapter(): Unit = {
     findActiveChapter(chapters.reverse) match {
-      case c: Chapter =>
+      case Some(c: Chapter) =>
         if (atLastPageOf(c)) {
           if (c == chapters.last) go(EndOfCard)
           else go(chapters(chapters.indexOf(c)+1))
         }
-      case c => go(c)
+      case Some(EndOfCard) => go(EndOfCard)
+      case Some(StartOfCard) =>
+        if (chapters.isEmpty) go(EndOfCard)
+        else go(chapters.head)
+      case None => fatal(f"active chapter was not found")
     }
   }
   def goFirstChapter(): Unit = go(chapters.head)
   def goLastChapter(): Unit = go(chapters.last)
   def castCurrentState(): Unit = {
-    flash.cast(Option(findActiveChapter(chapters).caption))
+    findActiveChapter(chapters) match {
+      case Some(c) => flash.cast(Option(c.caption))
+      case None => fatal(f"active chapter was not found")
+    }
   }
   def run(): Unit = {
     info("start")
