4a328b74ef394902dd83a3e5bbc21e259abf990a
rubyu <ruby.u.g@gmail.com>
2016-04-12 23:58:47 +0900

    add auto generate script for devices

---
+++ src/main/resources/SO-01G/post.js
@@ -0,0 +1,13 @@
+
+AnkiIframeViewerApp()
+  .audio("#detached-02 audio")
+  .chapter("#detached-03", "ランダムハウス英語辞典")
+  .chapter("#detached-04", "研究社 新英和大辞典")
+  .chapter("#detached-05", "リーダーズ・プラス")
+  .chapter("#detached-06", "英辞郎")
+  .chapter("#detached-07", "ロングマン現代英英辞典")
+  .chapter("#detached-08", "オックスフォード現代英英辞典")
+  .chapter("#detached-09", "斎藤和英大辞典")
+  .chapter("#detached-10", "ロングマン現代英英辞典")
+  .autoPlayAudio(false)
+  .run();
---
+++ src/main/resources/gen.ps1
@@ -0,0 +1,33 @@
+
+$7z = "C:\Program Files\7-Zip\7z.exe"
+
+$devices = @("PC", "Nexus7", "SO-01G")
+
+$cssA  = "anki-iframe-viewer.css"
+$_cssB = "post.css"
+$_cssC = "col-detach.css"
+
+$jsA   = "anki-iframe-viewer-opt.js"
+$_jsB  = "post.js"
+$_jsC  = "col-detach.js"
+
+foreach($device in $devices) {
+  $cssB = Join-Path $device $_cssB
+  $cssC = Resolve-Path (Join-Path $device $_cssC)
+  $jsB  = Join-Path $device $_jsB
+  $jsC  = Resolve-Path (Join-Path $device $_jsC)
+
+  Get-Content -Encoding UTF8 $cssA | Set-Content -Encoding UTF8 $cssC
+  Get-Content -Encoding UTF8 $cssB | Add-Content -Encoding UTF8 $cssC
+  Get-Content -Encoding UTF8 $jsA  | Set-Content -Encoding UTF8 $jsC
+  Get-Content -Encoding UTF8 $jsB  | Add-Content -Encoding UTF8 $jsC
+
+  $dst = "$device.zip"
+  if (Test-Path $dst) {
+    Remove-Item $dst
+  }
+
+  # `sources` for 7z, $cssC and $jsC here, must be an absolute path. Otherwise, 
+  # the generated zip file should contain the directory stracture to the source file.
+  Start-Process $7z -ArgumentList "a $dst $cssC $jsC"
+}
